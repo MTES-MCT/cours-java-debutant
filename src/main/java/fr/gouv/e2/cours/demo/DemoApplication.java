94157fecff916b9c01bb0b6fa1e5f66a7822c560
Tristan Robert <tristan.robert.44@gmail.com>
2020-03-25 15:05:34 +0100

    init projet

---
+++ src/main/java/fr/gouv/e2/cours/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package fr.gouv.e2.cours.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/gouv/e2/cours/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.gouv.e2.cours.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
